4bde94a293376c330dff6add986b930c864e8814
MarcFernandezParra <90846967+MarcFernandezParra@users.noreply.github.com>
2021-12-13 16:47:11 +0100

    Update app_data.sql

--- app_data.sql
+++ app_data.sql
@@ -16,3 +16,12 @@ INSERT INTO personajes (id_raza, id_clase, nombre, nivel, edad, altura, peso, vi
 (1, 1, 'Yoxi Ningel 2', 1, 50, 20, 1.0, 10, 10, 15, 2, 2, 4, 14, 8, 2),
 (1, 1, 'Yoxi Ningel 3', 1, 50, 20, 1.0, 10, 10, 15, 2, 2, 4, 14, 8, 2),
 (1, 1, 'Yoxi Ningel 4', 1, 50, 20, 1.0, 10, 10, 15, 2, 2, 4, 14, 8, 2);
+
+INSERT INTO public.objetos(
+	id, nombre, descripcion, peso, volumen, valor, equipable)
+	VALUES (1, 'armadura', 'como una roca', 32, '2x2', 30, true),
+	(2, 'espada', 'afilada', 22, '2x1', 15, true),
+	(3, 'pocion cura', 'cura', 2, '2x2', 1, false),
+	(4, 'toga', 'bata de ducha', 1, '2x2', 30, true),
+	(5, 'veneno', 'ta feo', 2, '2x2', 45, false);
+	
