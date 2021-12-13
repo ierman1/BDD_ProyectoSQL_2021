d57e6d7bb53543fe3e867a629e6cb87fa99eaf41
Guillem Caballé <guillemcaballe95@gmail.com>
2021-12-13 19:37:20 +0100

    He avançat amb la creacio de personatge

--- app_data.sql
+++ app_data.sql
@@ -6,10 +6,12 @@ INSERT INTO clases (nombre, descripcion, dado_vida, salvacion_1, salvacion_2) VA
 ('Hechicero', 'Fuego y magia y esas cosas', 4, 4, 5),
 ('Hechicero', 'Fuego y magia y esas cosas', 4, 4, 5);
 
-INSERT INTO razas (id_raza_padre, nombre, descripcion, velocidad_a_pie, velocidad_vuelo, velocidad_nado, ventajas, idiomas, altura) VALUES
-(NULL, 'Gnomo', 'Bajito y regordete, mazo viejo', 25, 0, 10, 'Ser superior', 'Comun y Gnomo', '0.8 - 1.2'),
-(NULL, 'Gnomo', 'Bajito y regordete, mazo viejo', 25, 0, 10, 'Ser superior', 'Comun y Gnomo', '0.8 - 1.2'),
-(NULL, 'Gnomo', 'Bajito y regordete, mazo viejo', 25, 0, 10, 'Ser superior', 'Comun y Gnomo', '0.8 - 1.2');
+INSERT INTO razas ("id", "id_raza_padre", "nombre", "descripcion", "velocidad_a_pie", "velocidad_nado", "velocidad_vuelo", "ventajas", "idiomas", "altura") VALUES (1, NULL, 'Gnomo', 'Bajito y regordete, mazo viejo', 25, 10, 0, 'Ser superior', 'Comun y Gnomo', '0.8 - 1.2');
+INSERT INTO razas ("id", "id_raza_padre", "nombre", "descripcion", "velocidad_a_pie", "velocidad_nado", "velocidad_vuelo", "ventajas", "idiomas", "altura") VALUES (2, 1, 'Gnomo leproso', 'En una piscina se convierte en aspirina', 15, 10, 0, 'Ser superior', 'Comun y Gnomo', '0.8 - 1.2');
+INSERT INTO razas ("id", "id_raza_padre", "nombre", "descripcion", "velocidad_a_pie", "velocidad_nado", "velocidad_vuelo", "ventajas", "idiomas", "altura") VALUES (3, 1, 'Gnomo de piedra', 'He''s rock solid', 20, 10, 0, 'Ser superior', 'Comun y Gnomo', '0.8 - 1.2');
+INSERT INTO razas ("id", "id_raza_padre", "nombre", "descripcion", "velocidad_a_pie", "velocidad_nado", "velocidad_vuelo", "ventajas", "idiomas", "altura") VALUES (4, NULL, 'Humano', 'Descripción del humano', 30, 10, 0, 'Negociación', 'Común y Humano', '1.6-1.9');
+INSERT INTO razas ("id", "id_raza_padre", "nombre", "descripcion", "velocidad_a_pie", "velocidad_nado", "velocidad_vuelo", "ventajas", "idiomas", "altura") VALUES (5, NULL, 'Elfo', 'Orejas puntiagudas y alto', 35, 10, 0, 'Immortalidad', 'Común y élfico', '1.7-2.1');
+
 
 INSERT INTO personajes (id_raza, id_clase, nombre, nivel, edad, altura, peso, vida_max, vida_actual, oro, fuerza, destreza, constitucion, inteligencia, sabiduria, carisma) VALUES
 (1, 1, 'Yoxi Ningel', 1, 50, 20, 1.0, 10, 10, 15, 2, 2, 4, 14, 8, 2),
